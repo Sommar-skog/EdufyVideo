dd053250d56682c160ebfcdd5b53269a1e3da6b9
Sommar-skog <amandaaronsson@hotmail.com>
2025-10-27 15:23:31 +0100

    added security config

--- src/main/java/com/example/EdufyVideo/configs/SecurityConfig.java
+++ src/main/java/com/example/EdufyVideo/configs/SecurityConfig.java
@@ -1,4 +1,32 @@
 package com.example.EdufyVideo.configs;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    //add converter
+
+    //ED-41-AA
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+         http
+                 .csrf(csrf -> csrf.disable())
+                 .headers(h -> h.frameOptions(f -> f.disable()))
+                 .authorizeHttpRequests(auth ->
+                         auth
+                                 .requestMatchers("/h2-console/**").permitAll()
+                                 .anyRequest().permitAll() //change later
+                 );
+                /* .oauth2ResourceServer(oauth2 ->
+                         oauth2
+                                 .jwt(jwt -> jwt.jwtAuthenticationConverter())
+                 );*/
+        return http.build();
+    }
 }
