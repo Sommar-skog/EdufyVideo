6422275c8d36d6ed877f1d0c1953f3f1b643769d
Sommar-skog <amandaaronsson@hotmail.com>
2025-10-30 14:35:51 +0100

    added endpoint i UserController getVideoCliById

--- src/main/java/com/example/EdufyVideo/controllers/UserController.java
+++ src/main/java/com/example/EdufyVideo/controllers/UserController.java
@@ -1,4 +1,20 @@
 package com.example.EdufyVideo.controllers;
 
-public class UserController {
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+    @RestController
+    @RequestMapping("/video")
+    public class UserController {
+
+/*    @GetMapping("/videoclip/{id}")
+    public ResponseEntity<VideoClipResponseDTO> getVideoClipById (@PathVariable Long id) {
+        return ResponseEntity.ok(videoClipService.getVideoClipById(id));
+    }*/
+
+
 }
