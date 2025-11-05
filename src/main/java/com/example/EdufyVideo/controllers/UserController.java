4514ce05dd6076570aac1968836584adc27c058a
Sommar-skog <amandaaronsson@hotmail.com>
2025-11-05 15:04:04 +0100

    added /videoclip-title endpoint-chain in UserController

--- src/main/java/com/example/EdufyVideo/controllers/UserController.java
+++ src/main/java/com/example/EdufyVideo/controllers/UserController.java
@@ -1,14 +1,33 @@
 package com.example.EdufyVideo.controllers;
 
+import com.example.EdufyVideo.models.dtos.VideoClipResponseDTO;
+import com.example.EdufyVideo.services.VideoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 //ED-78-AA
     @RestController
     @RequestMapping("/video")
     public class UserController {
 
+        private final VideoService videoService;
 
+        @Autowired
+        public UserController(VideoService videoService) {
+            this.videoService = videoService;
+        }
+
+    //ED-57-AA
+    @GetMapping("/videoclip-title")
+    public ResponseEntity<List<VideoClipResponseDTO>> getVideoClipByTitle (@RequestParam String title) {
+        return ResponseEntity.ok(videoService.getVideoClipByTitle(title));
+    }
 
 
 
--- src/main/java/com/example/EdufyVideo/repositories/VideoRepository.java
+++ src/main/java/com/example/EdufyVideo/repositories/VideoRepository.java
@@ -4,6 +4,7 @@ import com.example.EdufyVideo.models.enteties.VideoClip;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +12,7 @@ public interface VideoRepository  extends JpaRepository<VideoClip, Long> {
 
     //ED-252-AA
     Optional<VideoClip> findVideoClipByIdAndActiveTrue(Long id);
+
+    //ED-57-AA
+    List<VideoClip> findVideoClipByTitleContainingIgnoreCaseAndActiveTrue(String title);
 }
