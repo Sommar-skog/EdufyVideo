a0a65cb009679b923ded01e01b5503d0703be828
Sommar-skog <amandaaronsson@hotmail.com>
2025-11-14 08:16:00 +0100

    added role on AdminController.

--- src/main/java/com/example/EdufyVideo/controllers/AminController.java
+++ src/main/java/com/example/EdufyVideo/controllers/AminController.java
@@ -5,11 +5,13 @@ import com.example.EdufyVideo.services.PlaylistService;
 import com.example.EdufyVideo.services.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/video")
+@PreAuthorize("hasRole('video_admin')")
 public class AminController {
 
     //ED-78-AA
