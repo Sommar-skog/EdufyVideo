ddea3780b2ed430579d835bfbdff9b9f7e505046
Sommar-skog <amandaaronsson@hotmail.com>
2025-11-25 09:45:39 +0100

    updated interfaces

--- src/main/java/com/example/EdufyVideo/clients/ThumbClient.java
+++
@@ -1,9 +0,0 @@
-package com.example.EdufyVideo.clients;
-
-
-import com.example.EdufyVideo.models.enums.MediaType;
-
-//ED-345-AA
-public interface ThumbClient {
-    boolean createRecordeOfMedia(MediaType mediaType, Long mediaId, String mediaName);
-}
--- src/main/java/com/example/EdufyVideo/clients/UserClient.java
+++
@@ -1,8 +0,0 @@
-package com.example.EdufyVideo.clients;
-
-import com.example.EdufyVideo.models.dtos.UserDTO;
-
-//ED-345-AA
-public interface UserClient {
-    public UserDTO getUserBySub(String sub);
-}
