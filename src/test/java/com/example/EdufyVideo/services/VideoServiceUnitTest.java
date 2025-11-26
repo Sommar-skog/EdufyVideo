4ff09d2eeaf5442ecd70be6c083ea864ec037cb5
Sommar-skog <amandaaronsson@hotmail.com>
2025-11-26 13:21:31 +0100

    update of Exception and first 2 tests

--- src/test/java/com/example/EdufyVideo/services/VideoServiceUnitTest.java
+++ src/test/java/com/example/EdufyVideo/services/VideoServiceUnitTest.java
@@ -1,8 +1,13 @@
 package com.example.EdufyVideo.services;
 
 import com.example.EdufyVideo.clients.*;
+import com.example.EdufyVideo.exceptions.ResourceNotFoundException;
 import com.example.EdufyVideo.models.dtos.AddPlaylistDTO;
 import com.example.EdufyVideo.models.dtos.AddVideoClipDTO;
+import com.example.EdufyVideo.models.dtos.VideoClipResponseDTO;
+import com.example.EdufyVideo.models.dtos.mappers.VideoClipResponseMapper;
+import com.example.EdufyVideo.models.enteties.PlaylistEntry;
+import com.example.EdufyVideo.models.enteties.VideoClip;
 import com.example.EdufyVideo.repositories.VideoRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,9 +15,20 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 //ED-316-AA Unit tests
 @ExtendWith(MockitoExtension.class)
@@ -36,6 +52,8 @@ class VideoServiceUnitTest {
     private VideoServiceImpl videoService;
 
     private AddVideoClipDTO addVideoClipDTO;
+    private VideoClip video;
+    private VideoClipResponseDTO videoResponseDTO;
 
     @BeforeEach
     void setUp() {
@@ -43,17 +61,76 @@ class VideoServiceUnitTest {
                 "Test title",
                 "Test Description",
                 List.of(1L,2L),
-                "http://test.com",
+                "https://test.com",
                 LocalTime.of(0,10,32),
                 List.of(1L,2L),
                 List.of(1L)
         );
-    }
 
+        video = new VideoClip(
+                1L,
+                "Test title",
+                "https://test.com",
+                "Test Description",
+                LocalTime.of(0, 10, 32),
+                LocalDate.now(),
+                new HashMap<>(),
+                List.of(new PlaylistEntry()),
+                true
+        );
+
+        videoResponseDTO = new VideoClipResponseDTO();
+
+    }
 
     @Test
-    void getVideoClipById() {
+    void getVideoClipByIdShouldReturnAdminDtoWhenRoleIsAdmin(){
+
+        Collection<GrantedAuthority> roles = List.of(() -> "ROLE_video_admin");
+
+        when(mockVideoRepository.findById(1L)).thenReturn(Optional.of(video));
+        mockStatic(VideoClipResponseMapper.class).when(
+                () -> VideoClipResponseMapper.toDTOAdmin(video, mockCreatorClient, mockGenreClient)
+        ).thenReturn(videoResponseDTO);
+
+        VideoClipResponseDTO result = videoService.getVideoClipById(1L, roles);
+
+        assertEquals(videoResponseDTO, result);
+        verify(mockVideoRepository).findById(1L);
+
     }
+
+    @Test
+    void getVideoClipByIdShouldThrowWhenAdminVideoNotFound(){
+        Collection<GrantedAuthority> roles = List.of(() -> "ROLE_video_admin");
+
+        when(mockVideoRepository.findById(-1L)).thenReturn(Optional.empty());
+
+        ResourceNotFoundException exception = assertThrows(ResourceNotFoundException.class, () ->
+                videoService.getVideoClipById(-1L, roles));
+
+        assertEquals("VideoClip not found with id: -1", exception.getMessage());
+
+        verify(mockVideoRepository).findById(-1L);
+
+
+    }
+
+    @Test
+    void getVideoClipByIdShouldReturnUserDtoWhenRoleIsUser(){
+
+    }
+
+    @Test
+    void getVideoClipByIdShouldThrowWhenUserVideoNotFound(){
+
+    }
+
+    @Test
+    void getVideoClipByIdShouldUseCorrectRepositoryMethodDependingOnRole(){
+
+    }
+
 
     @Test
     void getVideoClipsByTitle() {
